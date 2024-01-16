eb02bb9ad2fc614496042c991e7d50cd63bc9d42
abiigiglabz <abiram.p@giglabz.com>
2024-01-16 15:53:24 +0530

    modified to check deployment output

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,65 @@
-import Hero from "@/Components/Hero";
+import Hero from "@/components/Hero";
 
 const Home = () => {
 
+  
+  const handleOnsignIn = async () => {
+    await signIn("google", {
+      redirect: "/globel-chat",
+      callbackUrl: "/globel-chat",
+    });
+  }
+
   return (
-    <Hero />
+     <section className="h-screen w-full overflow-hidden flex flex-col justify-center items-center relative bg-radial-gradient ">
+    <svg
+      className=" absolute lg:-top-28 left-1/2 transform -translate-x-1/2 2xl:w-5/12 xl:w-8/12 md:w-full w-[150vw] md:top-0 top-10"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      xmlLang="en"
+      viewBox="0 0 500 500"
+    >
+      <defs>
+        <path
+          id="textcircle"
+          d="M250,400a150,150 0 0,1 0,-300a150,150 0 0,1 0,300Z"
+          transform="rotate(114, 250, 250)"
+        />
+      </defs>
+
+      <g className="textcircle ">
+        <text>
+          <textPath
+            xlinkHref="#textcircle"
+            textLength="340"
+            className="text-4xl font-big"
+            style={{ fill: "#313131" }}
+          >
+            Globel - Chat
+          </textPath>
+        </text>
+      </g>
+    </svg>
+    {/* <Lottie
+      className="lg:h-3/4 md:h-2/4"
+      loop={false}
+      autoPlay={true}
+      animationData={Animi}
+    /> */}
+    <main className="absolute bottom-10 flex flex-col items-center" >
+        <p className=" md:p-0 p-2 text-center font-most text-myBrown mb-2">
+          Hay haii , welcome to Globel Chat ! , just login with your Gmail id
+          to get started.!
+        </p>
+        <h2
+          // onClick={handleOnsignIn}
+          className="font-few text-2xl myUnderline relative cursor-pointer"
+        >
+          Click to Login
+        </h2>
+    </main>
+
+  </section>
   );
 };
 
