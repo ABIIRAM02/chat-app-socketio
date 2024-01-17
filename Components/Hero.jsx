83cf989ef492ee57cd3dde75037599439431783c
abiigiglabz <abiram.p@giglabz.com>
2024-01-17 16:27:06 +0530

    last deployment

--- Components/Hero.jsx
+++ Components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
   };
 
   return !session ? (
-    <main className="absolute bottom-10 flex flex-col items-center">
+    <main className="relative bottom-0 flex flex-col items-center">
       <p className=" md:p-0 p-2 text-center font-most text-myBrown mb-2">
         Hay haii , welcome to Globel Chat ! , just login with your Gmail id to
         get started.!
